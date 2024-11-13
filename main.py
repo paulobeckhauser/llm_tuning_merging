9c2a4e7872352e7c4c2d33001d326762bb37d999
Paulo Beckhauser <pauloricardobeckhauser@gmail.com>
2024-11-13 11:18:03 +0100

    First test with a easy model - llama, 1b parameters

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-import os
-from dotenv import load_dotenv
-from huggingface_hub import hf_hub_download
+
+from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM
 
 def main():
+    
+    # set the model as tiny llama
+    model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
-    load_dotenv()
-    HUGGING_FACE_API_KEY = os.getenv("HUGGING_FACE_API_KEY")
-    print(HUGGING_FACE_API_KEY)
+    # get the tokenizer from the model
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # Load the model
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+
+    # Create a pipeline for text generation
+    generator = pipeline('text-generation', model=model, tokenizer=tokenizer, device=0) # Set device to 0 for GPU
+
+    # Generate text based on a prompt
+    prompt = "Who is Ada Lovelace?"
+    generated_text = generator(prompt, max_length=50)
+
+    # print the result
+    print(generated_text[0]['generated_text'])
+
+
 
 
 
