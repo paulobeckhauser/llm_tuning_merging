e3886867035461a90be0c0825e645a07619872f8
Paulo Beckhauser <pauloricardobeckhauser@gmail.com>
2024-11-15 23:47:25 +0100

    Mistral trial

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import os
-from dotenv import load_dotenv
-from huggingface_hub import hf_hub_download
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 def main():
 
-    load_dotenv()
-    HUGGING_FACE_API_KEY = os.getenv("HUGGING_FACE_API_KEY")
-    print(HUGGING_FACE_API_KEY)
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+    prompt = "My favourite condiment is"
+
+    model_inputs = tokenizer([prompt], return_tensors="pt").to("cuda")
+    model.to(device)
+
+    generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample=True)
+    tokenizer.batch_decode(generated_ids)[0]
+
+
 
 
 
